cb2450e2d7e3f97d7355beb93b776021b79eda6b
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-03 07:32:07 +0530

    Create noofspaces.cpp

---
+++ noofspaces.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+int count = 0;
+string s;
+getline(cin,s);
+int n=s.size();
+for (int i = 0; i<n; i++) //can't be i <= s.size() because s.at(s.size()) will result in overflow
+        {
+            if (s.at(i) ==' ')    count++;
+
+}
+    cout << endl << count;
+    cin.ignore();
+    return 0;
+    }
