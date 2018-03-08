f06f3917d02e1252fcf51eb69e8624cfa6d7f77c
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-08 17:53:03 +0530

    Create asciival.cpp

---
+++ asciival.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+class sample
+{
+	public:
+	char a;
+	int get()
+	{
+	cin>>a;
+	return 0;	
+	}
+	int calc()
+	{   
+	    
+	    cout<<int(a);
+	return 0;	
+	}
+};
+int main()
+{
+   sample s;
+   s.get();
+   s.calc();
+   return 0;
+}
