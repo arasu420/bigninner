0582df087bf60e92a8e25d27a3d68ad31034967c
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-11 13:45:25 +0000

    Create uppercase.cpp

---
+++ uppercase.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <locale>
+using namespace std;
+class SAMPLE{
+public:
+    locale loc;
+    string str;
+    int get()
+    {
+
+        cin>>str;
+        return 0;
+    }
+    int calc()
+    {
+       for (string::size_type i=0; i<str.length(); ++i)
+    cout << toupper(str[i],loc);
+       return 0;
+
+    }
+};
+int main()
+{
+    SAMPLE s;
+    s.get();
+    s.calc();
+    return 0;
+}
+
