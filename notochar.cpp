16d41a4f54cbc74349c3ca4e973efa6933e40231
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-11 14:36:35 +0000

    Create notochar.cpp

---
+++ notochar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+class SAMPLE{
+public:
+    int x;
+    char c;
+    int get()
+    {
+        cin>>x;
+
+        return 0;
+    }
+    int calc()
+    {
+       c=64+x;
+     cout<<c;
+        return 0;
+    }
+};
+int main()
+{
+    SAMPLE s;
+    s.get();
+    s.calc();
+    return 0;
+}
