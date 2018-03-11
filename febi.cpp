7297891d4b114b760f68ca15c11d18d46e234eeb
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-11 04:27:15 +0000

    Create febi.cpp

---
+++ febi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+class Feb{
+public:
+    int x,a=0,b=1,next;
+    int get()
+    {
+cin>>x;
+return 0;
+    }
+    int calc()
+    {
+       for(int i=1;i<=x;i++)
+        {
+                if ( i <= 1 )
+         next = i;
+      else
+      {
+         next = a + b;
+         a = b;
+         b = next;
+      }
+      cout<<next;
+        }
+        return 0;
+        }
+};
+int main()
+{
+    Feb s;
+    s.get();
+    s.calc();
+    return 0;
+}
