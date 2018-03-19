487dada730fc4bc869d9918b9053cdc59f442c4e
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-19 13:52:50 +0000

    Create addoddoreven.cpp

---
+++ addoddoreven.cpp
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int a,b;
+cin>>a>>b;
+int c=a+b;
+
+if(c%2==0)
+cout<<"even";
+else
+cout<<"odd";
+}
